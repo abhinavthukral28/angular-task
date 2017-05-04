f2fd8f5d25219b08bd72fdd39375b1379e62af7a
Abhinav Thukral <abhinav@abhinavthukral.com>
2017-05-04 04:00:39 -0400

    data interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Task } from "./model/task"
 @Component({
   moduleId: module.id,
   selector: 'app-root',
@@ -7,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  private tasks = [
+    new Task(
+      "Get started on laundry",
+      false
+    ),
+    new Task(
+      "Brush your teeth",
+      false
+    )
+  ]
 }
